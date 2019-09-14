9394a47a64b048a14a3ef385e7b8fbc36919e7fd
Evie-Skinner18 <eskinner@spartaglobal.com>
2019-09-14 19:10:15 +0100

    Required dotenv in js file and made a file called .env

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,6 @@
+// using dotenv for environment vars. Tells node to load anything that's in a file called .env into
+// an envirinement var
+require('dotenv').config();
 
 // create map instance
 var map = L.map('map').setView([51.505, -0.09], 13);
