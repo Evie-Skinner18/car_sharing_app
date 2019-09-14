5edc1e3d24a1873ab84d36e61e4d9ffbc0eefc33
Evie-Skinner18 <eskinner@spartaglobal.com>
2019-09-14 21:36:52 +0100

    Learnt how to properly add an .env file and .gitignore. You must make the .gitignore with the ignore rule and afterwards create the .env file, otherwise git will not ignore it

--- js/frontend.js
+++ js/frontend.js
@@ -1,3 +1,12 @@
 
 // create map instance
-var map = L.map('map').setView([51.505, -0.09], 13);
+// var map = L.map('map').setView([51.505, -0.09], 13);
+require('dotenv').config();
+const apiKey = process.env.API_KEY;
+
+var myMap = L.map('map');
+        myMap.setView([48.7965913, 2.3210938], 3);
+        L.tileLayer(`https://tile.jawg.io/jawg-sunny/{z}/{x}/{y}.png?access-token=${apiKey}`, {
+          attribution: '<a href="https://www.jawg.io" target="_blank">&copy; Jawg</a> | <a href="https://www.openstreetmap.org" target="_blank">&copy; OpenStreetMap</a>&nbsp;contributors',
+          maxZoom: 22
+        }).addTo(myMap);
