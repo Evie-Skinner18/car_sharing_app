42129edf05b4e59731a5f830988ef7b83a2224c4
Evie-Skinner18 <eskinner@spartaglobal.com>
2019-11-03 12:13:21 +0000

    Defined the root route to GET a view called map

--- app.js
+++ app.js
@@ -9,6 +9,12 @@ const apiKey = process.env.API_KEY;
 app.use(express.static('public'));
 app.set('view engine', 'ejs');
 
+// root route
+app.get('/', (req, res)=> {
+  res.render('map');
+})
+
+
 var myMap = L.map('map');
     myMap.setView([48.7965913, 2.3210938], 3);
     L.tileLayer(`https://tile.jawg.io/jawg-sunny/{z}/{x}/{y}.png?access-token=${apiKey}`, {
@@ -21,22 +27,22 @@ var myMap = L.map('map');
 
 
 // define the routes
-app.get('/', function(req, res){
-    response.send('Hello!');
-    console.log('Someone has made a request to /');
-});
+// app.get('/', function(req, res){
+//     response.send('Hello!');
+//     console.log('Someone has made a request to /');
+// });
 
-app.get('/cheese', function(req, res){
-    response.send('CHEEEEEEEESE');
-    console.log('Someone has made a request to /cheese');
-}
+// app.get('/cheese', function(req, res){
+//     response.send('CHEEEEEEEESE');
+//     console.log('Someone has made a request to /cheese');
+// }
 
-);
+// );
 
 
 
 // start the node server
 var port = process.env.PORT || 3000;
 app.listen(port, function () {
-  console.log('Server Has Started!');
+  console.log('Car sharing app has started!');
 });
